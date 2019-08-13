a5cb0f93b21c44730f84dff5389c4ac18d2fb11f
bartvanhoey <bartvanhoey@hotmail.com>
2019-08-13 22:13:25 +0200

    first commit

---
+++ src/LibraryApp.Application/MultiTenancy/ITenantAppService.cs
@@ -0,0 +1,11 @@
+﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using LibraryApp.MultiTenancy.Dto;
+
+namespace LibraryApp.MultiTenancy
+{
+    public interface ITenantAppService : IAsyncCrudAppService<TenantDto, int, PagedTenantResultRequestDto, CreateTenantDto, TenantDto>
+    {
+    }
+}
+
